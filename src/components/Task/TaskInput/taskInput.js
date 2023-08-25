f9e8b30225a891a8d068a98d8d867a3792b690ec
Artur Novikov <art14ek@tut.by>
2023-08-25 14:02:33 +0300

    TaskImput repair(deleted loose state)

--- src/components/Task/TaskInput/taskInput.js
+++ src/components/Task/TaskInput/taskInput.js
@@ -3,12 +3,6 @@ import React, { Component } from "react";
 import "./taskInput.css";
 
 export default class TaskInput extends Component {
-   constructor(props) {
-    super(props);
-        this.state = {
-            completed: props.completed || false
-        };
-   };
 
     onCheckboxClick = () => {
         const newCompletedStatus = !this.props.completed;
