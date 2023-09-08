be619a0e31b30905c0e57b40c1836b72b724853d
Artur Novikov <art14ek@tut.by>
2023-09-08 15:59:40 +0300

    deleted component TaslFilter

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import './footer.css';
-
-import TaskFilter from './TasksFilter';
 
 export default class Footer extends Component {
   static propTypes = {
@@ -17,12 +16,48 @@ export default class Footer extends Component {
     clearCompleted: () => {},
   };
 
+  state = {
+    activeButton: 'All',
+  };
+
+  setActiveButton = (filterType) => {
+    this.setState({
+      activeButton: filterType,
+    });
+    this.props.changeFilter(filterType);
+  };
+
   render() {
+    const { activeButton } = this.state;
+
     return (
       <footer className="footer">
         <span className="todo-count">{this.props.incompleteItems} items left</span>
         <ul className="filters">
-          <TaskFilter changeFilter={this.props.changeFilter} />
+          <li>
+            <button
+              className={classNames({ selected: activeButton === 'All' })}
+              onClick={() => this.setActiveButton('All')}
+            >
+              All
+            </button>
+          </li>
+          <li>
+            <button
+              className={classNames({ selected: activeButton === 'Active' })}
+              onClick={() => this.setActiveButton('Active')}
+            >
+              Active
+            </button>
+          </li>
+          <li>
+            <button
+              className={classNames({ selected: activeButton === 'Completed' })}
+              onClick={() => this.setActiveButton('Completed')}
+            >
+              Completed
+            </button>
+          </li>
         </ul>
         <button className="clear-completed" onClick={this.props.clearCompleted}>
           Clear completed
