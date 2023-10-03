f24835e2f66f305028ec42383f1ccdda66213249
Artur Novikov <art14ek@tut.by>
2023-10-03 14:50:55 +0300

    created hools for TaskList

--- src/components/TaskList/taskList.js
+++ src/components/TaskList/taskList.js
@@ -1,63 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './taskList.css';
 import Task from '../Task';
 
-export default class TaskList extends Component {
-  static propTypes = {
-    tasks: PropTypes.arrayOf(PropTypes.object).isRequired,
-    onTaskStatusChange: PropTypes.func.isRequired,
-    onDeleted: PropTypes.func,
-    onEdit: PropTypes.func,
-    onInputChange: PropTypes.func,
-    onInputSubmit: PropTypes.func,
-    onTick: PropTypes.func,
-    onStartTimer: PropTypes.func,
-    onStopTimer: PropTypes.func,
-  };
+function TaskList({
+  tasks,
+  onTaskStatusChange,
+  onDeleted = () => {},
+  onEdit = () => {},
+  onInputChange = () => {},
+  onInputSubmit = () => {},
+  onTick = () => {},
+  onStartTimer = () => {},
+  onStopTimer = () => {},
+}) {
+  const elements = tasks.map((task) => {
+    const { id, ...taskProps } = task;
+    return (
+      <Task
+        key={id}
+        id={id}
+        {...taskProps}
+        onStatusChange={(newCompletedStatus) => onTaskStatusChange(id, newCompletedStatus)}
+        onDeleted={() => onDeleted(id)}
+        onEdit={() => onEdit(id)}
+        onInputChange={(newInputValue) => onInputChange(id, newInputValue)}
+        onInputSubmit={() => onInputSubmit(id)}
+        onTick={() => onTick(id)}
+        onStartTimer={() => onStartTimer(id)}
+        onStopTimer={() => onStopTimer(id)}
+      />
+    );
+  });
 
-  static defaultProps = {
-    onDeleted: () => {},
-    onEdit: () => {},
-    onInputChange: () => {},
-    onInputSubmit: () => {},
-    onTick: () => {},
-    onStartTimer: () => {},
-    onStopTimer: () => {},
-  };
-
-  render() {
-    const {
-      tasks,
-      onTaskStatusChange,
-      onDeleted,
-      onEdit,
-      onInputChange,
-      onInputSubmit,
-      onTick,
-      onStartTimer,
-      onStopTimer,
-    } = this.props;
-    const elements = tasks.map((task) => {
-      const { id, ...taskProps } = task;
-      return (
-        <Task
-          key={id}
-          id={id}
-          {...taskProps}
-          onStatusChange={(newCompletedStatus) => onTaskStatusChange(id, newCompletedStatus)}
-          onDeleted={() => onDeleted(id)}
-          onEdit={() => onEdit(id)}
-          onInputChange={(newInputValue) => onInputChange(id, newInputValue)}
-          onInputSubmit={() => onInputSubmit(id)}
-          onTick={() => onTick(id)}
-          onStartTimer={() => onStartTimer(id)}
-          onStopTimer={() => onStopTimer(id)}
-        />
-      );
-    });
-
-    return <ul className="todo-list">{elements}</ul>;
-  }
+  return <ul className="todo-list">{elements}</ul>;
 }
+
+TaskList.propTypes = {
+  tasks: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onTaskStatusChange: PropTypes.func.isRequired,
+  onDeleted: PropTypes.func,
+  onEdit: PropTypes.func,
+  onInputChange: PropTypes.func,
+  onInputSubmit: PropTypes.func,
+  onTick: PropTypes.func,
+  onStartTimer: PropTypes.func,
+  onStopTimer: PropTypes.func,
+};
+
+export default TaskList;
