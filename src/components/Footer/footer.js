55c7b93d871d68cb16f98e04866a0be47465cbd8
Artur Novikov <art14ek@tut.by>
2023-10-03 16:05:56 +0300

    created hooks for Footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,68 +1,53 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import './footer.css';
 
-export default class Footer extends Component {
-  static propTypes = {
-    incompleteItems: PropTypes.number,
-    changeFilter: PropTypes.func,
-    clearCompleted: PropTypes.func,
+function Footer({ incompleteItems = 0, changeFilter = () => {}, clearCompleted = () => {} }) {
+  const [activeButton, setActiveButtonState] = useState('All');
+
+  const setActiveButton = (filterType) => {
+    setActiveButtonState(filterType);
+    changeFilter(filterType);
   };
 
-  static defaultProps = {
-    incompleteItems: 0,
-    changeFilter: () => {},
-    clearCompleted: () => {},
-  };
-
-  state = {
-    activeButton: 'All',
-  };
-
-  setActiveButton = (filterType) => {
-    this.setState({
-      activeButton: filterType,
-    });
-    this.props.changeFilter(filterType);
-  };
-
-  render() {
-    const { activeButton } = this.state;
-
-    return (
-      <footer className="footer">
-        <span className="todo-count">{this.props.incompleteItems} items left</span>
-        <ul className="filters">
-          <li>
-            <button
-              className={classNames({ selected: activeButton === 'All' })}
-              onClick={() => this.setActiveButton('All')}
-            >
-              All
-            </button>
-          </li>
-          <li>
-            <button
-              className={classNames({ selected: activeButton === 'Active' })}
-              onClick={() => this.setActiveButton('Active')}
-            >
-              Active
-            </button>
-          </li>
-          <li>
-            <button
-              className={classNames({ selected: activeButton === 'Completed' })}
-              onClick={() => this.setActiveButton('Completed')}
-            >
-              Completed
-            </button>
-          </li>
-        </ul>
-        <button className="clear-completed" onClick={this.props.clearCompleted}>
-          Clear completed
-        </button>
-      </footer>
-    );
-  }
+  return (
+    <footer className="footer">
+      <span className="todo-count">{incompleteItems} items left</span>
+      <ul className="filters">
+        <li>
+          <button className={classNames({ selected: activeButton === 'All' })} onClick={() => setActiveButton('All')}>
+            All
+          </button>
+        </li>
+        <li>
+          <button
+            className={classNames({ selected: activeButton === 'Active' })}
+            onClick={() => setActiveButton('Active')}
+          >
+            Active
+          </button>
+        </li>
+        <li>
+          <button
+            className={classNames({ selected: activeButton === 'Completed' })}
+            onClick={() => setActiveButton('Completed')}
+          >
+            Completed
+          </button>
+        </li>
+      </ul>
+      <button className="clear-completed" onClick={clearCompleted}>
+        Clear completed
+      </button>
+    </footer>
+  );
 }
+
+Footer.propTypes = {
+  incompleteItems: PropTypes.number,
+  changeFilter: PropTypes.func,
+  clearCompleted: PropTypes.func,
+};
+
+export default Footer;
